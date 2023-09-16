fb7e002b7ae5ef0ebc3a95acafe8e480960d5e9a
amutsamoses <amutsamoses@gmail.com>
2023-09-16 04:52:34 -0700

    Simple shell project using C

---
+++ get_sigint.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_sigint - function that handle the crtl + c call in prompt
+ *
+ * @sig: Signal handler
+ *
+ * Return: nothing
+ */
+void get_sigint(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n^-^ ", 5);
+}
